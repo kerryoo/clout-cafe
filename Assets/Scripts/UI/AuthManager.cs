c87915502dca8e124d9a63af44bea3064ced05e7
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2021-04-27 22:25:39 -0500

    updated model and finished welcome/login screens

--- Assets/Scripts/UI/AuthManager.cs
+++ Assets/Scripts/UI/AuthManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Amazon;
+using Amazon.CognitoIdentity;
 using UnityEngine;
 
 public class AuthManager : MonoBehaviour
@@ -7,12 +9,17 @@ public class AuthManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityInitializer.AttachToGameObject(this.gameObject);
+        CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+            "us-east-1:6c841b02-010e-4c58-b410-50b08d746f87", // Identity pool ID
+             RegionEndpoint.USEast1);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
+
+
     }
 }
