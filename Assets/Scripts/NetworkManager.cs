b3f688c66bec6671a081503b4ebf2b75561c8faf
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2021-05-09 22:52:56 -0500

    added couchdb access

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+public class NetworkManager : MonoBehaviour
+{
+    private readonly string address = "http://admin:password@3.231.58.128:5984/";
+    private void Start()
+    {
+        string IP = GetLocalIPAddress();
+        Debug.Log(IP);
+        //StartCoroutine(GetRequest("http://admin:password@3.231.58.128:5984/"));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
